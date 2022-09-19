c428959c20db1216c99d608ea8a4c991ad7b2461
Christy Sung <christysung@ChristytekiMacBook-Pro.local>
2022-09-19 15:55:40 -0700

    add js

--- examples/js/jquery.js
+++ examples/js/jquery.js
@@ -13,4 +13,15 @@ $(()=>{
             .siblings("dd").slideUp();
     });
 
+    /* Tab group */
+    $(".tabgroup .tab").on("click", function(e){
+        let id= $(this).index();
+
+        $(this).addClass("active")
+            .siblings().removeClass("active")
+
+        $(this).closest(".tabgroup")
+            .find(".content").eq(id).addClass("active")
+            .siblings().removeClass("active")
+    })
 });

b0f2edc130126c9c5438f4723286895c73cc2f57
Christy Sung <christysung@ChristytekiMacBook-Pro.local>
2022-10-10 21:06:31 -0700

    support onboarding page

--- src/js/signin.js
+++ src/js/signin.js
@@ -9,12 +9,17 @@ const checkSigninForm = () => {
     if (userval === "user" && passval === "pass") {
         // Logged In
         console.log("Success");
+
+        // If user logged in, set sessionStorage.userId.
         sessionStorage.userId = 1;
 
         $("#signin-form")[0].reset();
     } else {
         // Not Logged In
         console.log("Failure");
+
+        // If not logged in, erase sessionStorage.userId.
+        // Thus, sessionStorage.userId = undefined.
         sessionStorage.removeItem("userId");
 
         $(".warning").css("display", "block");
@@ -26,17 +31,35 @@ const checkSigninForm = () => {
 
 
 const checkUserId = () => {
-    const pages = ["#signin-page", "#signup-page", ""];
-
+    // We updated the `sessionStorage.userId` in `checkSigninForm`.
+    // Thus, we can read it to see it user is logged-in or not.
     if (sessionStorage.userId === undefined) {
-        // not logged in
-        if (!pages.some(p => p === window.location.hash)) {
+        // User is not logged in.
+
+        // If We are at 
+        //   sign-up page now and click confirm or
+        //   sign-in page but failed to logged in
+        // go to sign-in page directly without going to onboarding page.
+        //
+        // When current url is "https://christysung02.com/aau/ixd617/#signup-page",
+        // window.location.hash would be "#signup-page"
+        if (window.location.hash === "#signup-page" || window.location.hash === "#signin-page") {
             $.mobile.navigate("#signin-page");
+            return; //end
         }
+
+        // Otherwise, go to onboarding page first.
+        $.mobile.navigate("#onboarding-page");    
+
+        // Wait for 1.5 sec and then go to sign-in page.
+        setTimeout(() => {
+            console.log("After 1.5 sec.")
+            $.mobile.navigate("#signin-page");
+        },1500)
     } else {
         // logged in
-        if (pages.some(p => p === window.location.hash)) {
-            $.mobile.navigate("#recent-page");
-        }
+        $.mobile.navigate("#recent-page");
     }
 }
+
+
